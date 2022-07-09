29b2ed54b43e057d9fb0827374c76b9566741836
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2022-07-08 23:29:43 -0300

    criado verificações para tentar pegar o value das variaveis ambiente no umbler.

--- restrito/conexao.php
+++ restrito/conexao.php
@@ -1,18 +1,47 @@
 <?php
-    //$server = "";
-$server = getenv("server");
-    //$user = "";
-$user = getenv("user");
-    //$pass = "";
-$pass = getenv("pass");
-    //$bd = "";
-$bd = getenv("bd");
 
-    // $server = "localhost";
-    // $user = "root";
-    // $pass = "";
-    // $bd = "empresa";
-   
+    /**
+     * ALTERAR NOME DAS VARIAVEIS PARA MAIUSCULO NO UMBLER (PARA SEGUIR O PADRÃO DOS NOMES DE VARIÁVEIS(CHAVES))
+     * FOI ALTERADO OS NOMES DAS VARIÁVEIS PARA SER MAIS ESPECIFICO E
+     * COM ISSO DIMINUIR A CHANCE DE CONFLITAR COM UMA OUTRA VARIÁVEL
+     * Sugestão de nomes(key) para as variáveis: "SERVER_PODEMOS", "SERVER_USER_PODEMOS" e assim por diante;
+     */
+
+    $server = "";
+    $user = "";
+    $pass = "";
+    $bd = "";
+
+    /**
+     * if para verificar e decidir metodo que conseguirá pegar o valor das variáveis de ambiente.
+     */
+    if (isset($_SERVER['SERVER_PODEMOS'])){
+        $server = $_SERVER['SERVER_PODEMOS'];
+       // echo("<script>console.log('USAR : $_SERVER" . $server . "');</script>"); Descomentar linha para testes locais ou para saber qual método correto que pega as variáveis do UMBLER
+    } else {
+        $server = getenv("SERVER_PODEMOS");
+       // echo("<script>console.log('Variaveis: getenv("")" . $server . "');</script>"); Descomentar linha para testes locais ou para saber qual método correto que pega as variáveis do UMBLER
+    }
+
+    if (isset($_SERVER['SERVER_USER_PODEMOS'])){
+        $user = $_SERVER['SERVER_USER_PODEMOS'];
+    } else {
+        $user = getenv("SERVER_USER_PODEMOS");
+    }
+
+    if (isset($_SERVER['SERVER_PASS_PODEMOS'])){
+        $pass = $_SERVER['SERVER_PASS_PODEMOS'];
+    } else {
+        $pass = getenv("SERVER_PASS_PODEMOS");
+    }
+
+    if (isset($_SERVER['SERVER_BD_PODEMOS'])){
+        $bd = $_SERVER['SERVER_BD_PODEMOS'];
+    } else {
+        $bd = getenv("SERVER_BD_PODEMOS");
+    }
+
+
     $conn = mysqli_connect($server, $user, $pass, $bd);
 
     // if ($conn = mysqli_connect($server, $user, $pass, $bd)) {
